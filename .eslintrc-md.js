c2440294eb0214183bcb41ebc7040f004226b358
AndersDJohnson <AndersDJohnson@users.noreply.github.com>
2017-07-07 10:43:43 -0500

    docs: add eslint markdown

---
+++ .eslintrc-md.js
@@ -0,0 +1,13 @@
+module.exports = {
+    extends: 'standard',
+    globals: {
+      actionTypes: false,
+      createReducer: false
+    },
+    plugins: [
+      'markdown'
+    ],
+    rules: {
+      'no-unused-expressions': 0
+    }
+}
