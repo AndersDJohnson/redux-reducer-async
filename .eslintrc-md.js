006d7f02416df3e3ff973cbf7fdc3b3a2276bfd6
Anders D. Johnson <AndersDJohnson@users.noreply.github.com>
2017-08-15 12:31:21 -0500

    chore(lint): allow unused vars in md

--- .eslintrc-md.js
+++ .eslintrc-md.js
@@ -1,13 +1,14 @@
 module.exports = {
-    extends: 'standard',
-    globals: {
-      actionTypes: false,
-      createReducer: false
-    },
-    plugins: [
-      'markdown'
-    ],
-    rules: {
-      'no-unused-expressions': 0
-    }
+  extends: 'standard',
+  globals: {
+    actionTypes: false,
+    createReducer: false
+  },
+  plugins: [
+    'markdown'
+  ],
+  rules: {
+    'no-unused-expressions': 0,
+    'no-unused-vars': 0
+  }
 }
