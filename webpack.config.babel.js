a5782ef11ef6bdf4aa1dd6919c478e8d8a4cc606
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2019-04-25 20:28:39 -0500

    build: webpack library

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -4,7 +4,9 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'index.js'
+    filename: 'index.js',
+    library: 'reduxReducerAsync',
+    libraryTarget: 'umd'
   },
   module: {
     rules: [
